b0f09049069161c9e5b6bf0691a92f73070a7a59
Srikanth Sharma <srikanth.sharma@freshworks.com>
2019-11-20 10:06:07 +0530

    Add simple server with dockerfile

---
+++ server/src/app.py
@@ -0,0 +1,12 @@
+from os import environ
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def getData():
+    return 'ok'
+
+if __name__ == '__main__':
+    port = environ['PORT'] if 'PORT' in environ else '8080'
+    app.run('0.0.0.0', port, debug=True)
